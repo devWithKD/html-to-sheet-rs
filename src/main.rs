37cdd172f3273bd19ad6006d79a988bc72d47dcb
Kedar Joshi <kedarjo2002@gmail.com>
2024-09-02 02:06:10 +0530

    Data Struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use html_to_sheets_rust::{
+    EXPECTED_SHEET1, EXPECTED_SHEET2, EXPECTED_SHEET3, EXPECTED_SHEET4, EXPECTED_SHEET5,
+};
 use scraper::selectable::Selectable;
 use scraper::{Html, Selector};
-use std::env;
-use std::error::Error;
-use std::fs;
-use std::process;
+use serde::{Deserialize, Serialize};
+use std::{env, error::Error, fmt, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,21 +21,50 @@ fn main() {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Form {
+    sheet1: Vec<(String, String)>,
+    sheet2: Vec<(String, String)>,
+    sheet3: Vec<(String, String)>,
+    sheet4: Vec<(String, String)>,
+    sheet5: Vec<(String, String)>,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl Error for CustomError {}
+
+#[derive(Default, Debug)]
+struct Data {
+    sheet1: Vec<Vec<String>>,
+    sheet2: Vec<Vec<String>>,
+    sheet3: Vec<Vec<String>>,
+    sheet4: Vec<Vec<String>>,
+    sheet5: Vec<Vec<String>>,
+}
+
 struct Config {
     input: String,
-    output: String,
+    // output: String,
 }
 
 impl Config {
     fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
+        if args.len() < 2 {
             return Err("Insufficient Arguments!");
         } else if args.len() > 4 {
             return Err("Too many arguments!");
         }
         Ok(Config {
             input: args[1].clone(),
-            output: args[2].clone(),
+            // output: args[2].clone(),
         })
     }
 
@@ -46,27 +76,28 @@ impl Config {
         let row_selector = Selector::parse("tr").unwrap();
         let col_selector = Selector::parse("th,td").unwrap();
 
-        for table in document.select(&table_selector) {
+        let mut data = Data::default();
+
+        for (sheet_num, table) in document.select(&table_selector).enumerate() {
+            let sheet_ref = match sheet_num {
+                0 => &mut data.sheet1,
+                1 => &mut data.sheet2,
+                2 => &mut data.sheet3,
+                3 => &mut data.sheet4,
+                4 => &mut data.sheet5,
+                _ => return Err(Box::new(CustomError("Sheet Limit Exceeded".into()))),
+            };
+
             for row in table.select(&row_selector) {
-                // let row_data:Vec<_> = Vec::new();
-                let cells: Vec<_> = row
+                let cells: Vec<String> = row
                     .select(&col_selector)
-                    .map(|cell| cell.text().collect::<Vec<_>>().concat())
+                    .map(|cell| cell.text().collect::<Vec<_>>().concat().trim().to_owned())
                     .collect();
-                let cells: Vec<String> = cells
-                    .iter()
-                    .map(|cell| {
-                        if cell.starts_with("$") {
-                            let cell = cell.replace("$", "");
-                            let cell = cell.replace(",", "");
-                            return cell.to_owned();
-                        }
-                        cell.to_owned()
-                    })
-                    .collect();
-                println!("{:#?}", cells)
+                sheet_ref.push(cells);
             }
         }
+        println!("{:#?}", data);
+
         Ok(())
     }
 }
